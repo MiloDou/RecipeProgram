b5bbc1284a245d4dc837a6cc6dde667f9be8d0d2
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-07-31 09:18:11 -0600

    Se terminaron cada uno de las funciones junto al método

--- 30.7/Recetas.cs
+++ 30.7/Recetas.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,12 @@ namespace _30._7
 {
     public class Recetas
     {
-        
 
-        public int[] Id {  get; set; }
+
+        public int[] Id { get; set; }
         public string[] Name { get; set; }
-        public string[] Description {  get; set; }
-        public int Indice {  get; set; }
+        public string[] Description { get; set; }
+        public int Indice { get; set; }
         public Recetas(int[] id, string[] name, string[] description, int index)
         {
             Id = id;
@@ -23,12 +24,68 @@ namespace _30._7
         }
         public int NewRecipe(int[] id, string[] name, string[] description, int index)
         {
+            Console.Clear();
+            Console.WriteLine("------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("       ADD NEW PRODUCT");
+            Console.ResetColor();
+            Console.WriteLine("------------------------------\n");
+            try
+            {
+                Console.WriteLine("Name recipe: ");
+                name[index] = Console.ReadLine();
+                Console.WriteLine("Description: ");
+                description[index] = Console.ReadLine();
+            }
+            catch (FormatException MessageError1)
+            {
+
+                Console.WriteLine("Error found: " + MessageError1);
+            }
+            Console.WriteLine("ID recipe: ");
+            id[index] = Convert.ToInt32(Console.ReadLine());
 
 
-            return Id[0];
+            return index++;
         }
+        public void ConsultRecipe()
+        {
+            Console.WriteLine("------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("       CONSULT PRODUCT");
+            Console.ResetColor();
+            Console.WriteLine("------------------------------\n");
+            Console.WriteLine("Write name recipe: ");
+            string RecipeConsult = Console.ReadLine();
+            for (int i = 0; i < Indice; i++)
+            {
+                if (Name[i] == RecipeConsult)
+                {
+                    Console.WriteLine($"ID: {Id[i]}");
+                    Console.WriteLine($"Recipe Name: {Name[i]}");
+                    Console.WriteLine($"Description: {Description[i]}");
+                }
+                else
+                    Console.WriteLine("Recipe NO FOUND");
+            }
+
+        }
+        public void ShowResume()
+        {
+            Console.WriteLine("------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("            RESUMÉ  ");
+            Console.ResetColor();
+            Console.WriteLine("------------------------------\n");
+
+            for (int i = 0; i < Indice; i++)
+            {
+                Console.WriteLine("--------------------------------------------------------------");
+                Console.WriteLine($"ID: {Id[i]}, Name: {Name[i]}, Desciption: { Description[i]}");
+                Console.WriteLine("--------------------------------------------------------------");
+            }
 
 
-
+        }
     }
 }
