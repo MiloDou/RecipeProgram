e1f86d865d7b45199b7966a54ddeef8fb48d443a
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-07-31 08:59:40 -0600

    Agregar archivos de proyecto.

---
+++ 30.7/Recetas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._7
+{
+    public class Recetas
+    {
+        
+
+        public int[] Id {  get; set; }
+        public string[] Name { get; set; }
+        public string[] Description {  get; set; }
+        public int Indice {  get; set; }
+        public Recetas(int[] id, string[] name, string[] description, int index)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Indice = index;
+        }
+        public int NewRecipe(int[] id, string[] name, string[] description, int index)
+        {
+
+
+            return Id[0];
+        }
+
+
+
+    }
+}
